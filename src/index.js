8d25dc823c845f319bef45f7331c83b25cbe00f1
ArtakSaakyan <artak.saakyan.99@mail.ru>
2022-12-28 19:17:00 +0500

    feat: add index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function toReadable (number) {
+// module.exports = function toReadable (number) {
   
+// }
+
+function toReadable (number) {
+  return number
 }
+
+console.log(toReadable(1)); // Will return 'one'
+console.log(toReadable(997)); //will return 'nine hundred ninety seven'
